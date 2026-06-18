6e2bf454229453724d7a11b78cff05f4e37d6d31
CC-Real <danf26.cc@gmail.com>
2026-06-18 11:08:05 -0500

    Add files via upload

---
+++ Problem Set 1.1/onePrint.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void){
+    printf("*****\n**|**\n*|.|*\n|...|\n.....");
+    return 0;
+}
+// Making sure I know that I can use line breaks within a single printf statement
