fd249b1b447a049242ddb8d6b712f4e7987b90ef
CC-Real <danf26.cc@gmail.com>
2026-06-18 10:46:56 -0500

    Fix formatting and update greeting in newLines.c

--- newLines.c
+++ newLines.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void){
-    printf("hello world,\n");
+    printf("hello, world,\n");
     //Learned about the \n function to create a separate line for the next printf function!
     printf("have fun with this course!");
     return 0;
