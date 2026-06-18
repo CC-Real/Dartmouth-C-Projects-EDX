53f41cb3465276d5ddee2a83d841a8cc4b4f0a49
CC-Real <danf26.cc@gmail.com>
2026-06-18 11:00:50 -0500

    Delete severalLines.c

--- severalLines.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>  
-int main(void){
-    printf("I already know how to:\n");
-    printf("- Print text to the screen.\n");
-    printf("- Start a new line.\n");
-    printf("- Fix errors.");
-    return 0;
-}
-// We prove that we know how to add several sentences on different lines.
