977834cc6c298bf714dee17294a176610e4d7666
CC-Real <danf26.cc@gmail.com>
2026-06-22 10:35:20 -0500

    Add files via upload

---
+++ Problem Set 1.1/quotesInQuotes.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("dennis ritchie said\n\"The only way to learn a new programming language is by writing programs in it.\"");
+    return 0;
+}
+/*
+Learning how to use quotes inside quotes using \" Hello\"
+*/
